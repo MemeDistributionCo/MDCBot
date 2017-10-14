d961ba3ad081322569328748c8d9086da05cf86b
Zach | xDest <destroyer7712@gmail.com>
2017-10-14 14:08:46 -0400

    Progress on sd role check

--- MDCBot/src/main/java/com/mdc/bot/command/ShutdownCommand.java
+++ MDCBot/src/main/java/com/mdc/bot/command/ShutdownCommand.java
@@ -1,6 +1,11 @@
 package com.mdc.bot.command;
 
+import java.util.List;
+
 import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Role;
 
 public class ShutdownCommand implements Command {
 
@@ -12,9 +17,36 @@ public class ShutdownCommand implements Command {
 	@Override
 	public void action(CommandSet s) {
 		MessageBuilder mb = new MessageBuilder();
+		Role adminRole;
+		try {
+			adminRole = s.getMessageReceivedEvent().getGuild().getRolesByName("sd", true).get(0);
+		} catch (IndexOutOfBoundsException e) {
+			//Catching index out on this bc getRoles might rerturn null
+			s.getMessageReceivedEvent().getTextChannel().sendMessage("Role not found");
+			adminRole = null;
+		}
+		Guild server = s.getMessageReceivedEvent().getGuild();
+		//Do null check in if statement bc
+		if(adminRole != null && !containsMember(server.getMembersWithRoles(adminRole), server.getMember(s.getSender()))) {
+			s.getMessageReceivedEvent().getTextChannel().sendMessage("TTTTTTEST");
+		}
 		mb.append("Au revoir ").append(s.getMessageReceivedEvent().getGuild().getEmotesByName("thecool", true).get(0));
 		s.getMessageReceivedEvent().getTextChannel().sendMessage(mb.build()).complete();
 		s.getMessageReceivedEvent().getJDA().shutdown();
+	}
+	
+	//I don't think the objects are equal so im making this to compare by id
+	private boolean containsMember(List<Member> memberList, Member m) {
+		if(memberList == null || m == null) {
+			return false;
+		}
+		for(Member member : memberList) {
+			if(member.getUser().getIdLong() == m.getUser().getIdLong()) {
+				//Match
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
