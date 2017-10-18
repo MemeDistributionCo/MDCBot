b6f30954b725303221db6b434b307ff9db47de35
Zach | xDest <destroyer7712@gmail.com>
2017-10-18 17:01:45 -0400

    Added conversion method

--- MDCBot/src/main/java/com/mdc/bot/util/PermUtil.java
+++ MDCBot/src/main/java/com/mdc/bot/util/PermUtil.java
@@ -1,10 +1,17 @@
 package com.mdc.bot.util;
 
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.User;
 
 public class PermUtil {
 
+	/**
+	 * Return whether the user has the "sd" role (Server Developer)
+	 * @param a The member
+	 * @return true or false
+	 */
 	public static boolean isUserSD(Member a) {
 		for(Role r : a.getRoles()) {
 			if (r.getName().equalsIgnoreCase("sd")) {
@@ -14,4 +21,13 @@ public class PermUtil {
 		return false;
 	}
 	
+	/**
+	 * Returns the Member from the Guild provided, assuming the user exists in it.
+	 * @param u The user
+	 * @param g The guild
+	 * @return The member, or null
+	 */
+	public static Member userToMember(User u, Guild g) {
+		return g.getMember(u);
+	}
 }
