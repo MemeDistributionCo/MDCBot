d3ec77089c4d530c300720c597671e1c2a456759
Zach | xDest <destroyer7712@gmail.com>
2017-10-18 18:10:51 -0400

    Added command list command

--- MDCBot/src/main/java/com/mdc/bot/command/CommandLabel.java
+++ MDCBot/src/main/java/com/mdc/bot/command/CommandLabel.java
@@ -1,7 +1,7 @@
 package com.mdc.bot.command;
 
 public enum CommandLabel {
-	HELLO("hello", HelloCommand.class),SHUTDOWN("shutdown",ShutdownCommand.class),COOL("cool",CoolCommand.class),TTS("tts",TTSCommand.class),HELP("help",HelpCommand.class),FSpeak("fspeak",FSpeakCommand.class);
+	HELLO("hello", HelloCommand.class),SHUTDOWN("shutdown",ShutdownCommand.class),COOL("cool",CoolCommand.class),TTS("tts",TTSCommand.class),HELP("help",HelpCommand.class),FSpeak("fspeak",FSpeakCommand.class),ListCommand("commands",ListCommand.class);
 	
 	private String label;
 	private Class<?> commandClass;
