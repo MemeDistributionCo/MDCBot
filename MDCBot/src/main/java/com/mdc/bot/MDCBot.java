facd7b3b51d3e83eb39a217cc6e6cff1af3b86db
Zach | xDest <destroyer7712@gmail.com>
2017-10-19 21:05:07 -0400

    Change MDCBot to be self contained

--- MDCBot/src/main/java/com/mdc/bot/MDCBot.java
+++ MDCBot/src/main/java/com/mdc/bot/MDCBot.java
@@ -1,5 +1,7 @@
 package com.mdc.bot;
 
+import javax.security.auth.login.LoginException;
+
 import com.mdc.bot.reaction.CoolReaction;
 
 import net.dv8tion.jda.core.AccountType;
@@ -7,6 +9,7 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class MDCBot {
 	
@@ -14,62 +17,113 @@ public class MDCBot {
 		//Please read the readme on the github repo (https://github.com/MemeDistributionCo/MDCBot)
 		//Please do not make any changes on master or testing-merge-here
 		//Can be found at https://github.com/DV8FromTheWorld/JDA
-		private static JDA jda;
-		private static final String LIVE_TOKEN = "MzY3NDk2MzI5OTQ3MTE5NjE4.DL8SHQ.nW_rtXFgD7ytS3j7_lzZqxb4D5c", TEST_TOKEN = "MzY4MjE2MjU0Njg0NTk0MTc2.DMGvnw.gm57DR4Ado7zYE9M75zBI9x-38c";
+		
+		//private static JDA jda;
+		//private static final String LIVE_TOKEN = "MzY3NDk2MzI5OTQ3MTE5NjE4.DL8SHQ.nW_rtXFgD7ytS3j7_lzZqxb4D5c", TEST_TOKEN = "MzY4MjE2MjU0Njg0NTk0MTc2.DMGvnw.gm57DR4Ado7zYE9M75zBI9x-38c";
 		
 		//Bot settings
 		public static boolean ttsMode;
 		
 		
-		//Version 1.0.0
+		private JDA jdaInstance;
+		private String token;
+		private boolean ttsEnabled;
+		private boolean loggedIn;
 		
-		public static void main(String[] args) {
-			ttsMode = false;
-			try {
-				//Creates a JDA (Java Discord API) instance of "Bot" type with the bot token below. Builds account with blocking (freezes until finished, versus aSync)
-				/*
-				 * Switch TEST_TOKEN to LIVE_TOKEN when merging to master
-				 */
-				String tokenToUse;
-				if(args.length == 0) {
-					tokenToUse = TEST_TOKEN;
-				} else {
-					try {
-						boolean useLive = Boolean.parseBoolean(args[0]);
-						if(useLive) {
-							tokenToUse = LIVE_TOKEN;
-						} else {
-							tokenToUse = TEST_TOKEN;
-						}
-					} catch (Exception e) {
-						System.out.println("Error--Not a boolean value");
-						tokenToUse = TEST_TOKEN;
-					}
-				}
-				jda = new JDABuilder(AccountType.BOT).setToken(tokenToUse).buildBlocking();
-				//Command listener
-				jda.addEventListener(new MessageListener());
-				
-				/*
-				 * Reactions and other listeners
-				 */
-				jda.addEventListener(new CoolReaction());
-				
-				jda.setAutoReconnect(true);
-			} catch (Exception e) {
-				System.out.println("There was an error enabling the bot");
-				e.printStackTrace();
-			}
+		
+		/**
+		 * Attempts to construct a Bot with the provided token.
+		 * @param token Token
+		 * @throws LoginException
+		 * @throws IllegalArgumentException
+		 * @throws InterruptedException
+		 * @throws RateLimitedException
+		 */
+		public MDCBot(String token)  {
+			this.token = token;
+			ttsEnabled = false;
+			jdaInstance = null;
+			this.loggedIn = false;
 		}
 		
+		public void login() throws LoginException,IllegalArgumentException,InterruptedException, RateLimitedException {
+			jdaInstance = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
+			
+			//Command listener
+			jdaInstance.addEventListener(new MessageListener(this));
+			
+			/*
+			 * Reactions and other listeners
+			 */
+			jdaInstance.addEventListener(new CoolReaction(this));
+			
+			jdaInstance.setAutoReconnect(true);
+			
+			//if it makes it here, the bot is logged in
+			this.loggedIn = true;
+		}
+		
+		public String getToken() {
+			return token;
+		}
+		
+		public JDA getJDAInstance() {
+			return this.jdaInstance;
+		}
+		
+		public void setTTS(boolean v) {
+			this.ttsEnabled = v;
+		}
+		
+		public boolean isTTS() {
+			return this.ttsEnabled;
+		}
+		
+		public boolean isLoggedIn() {
+			return this.loggedIn;
+		}
+
 		/**
 		 * Sends a completed message to the text channel provided.
 		 * @param tc
 		 * @param message
 		 */
-		public static void sendMessage(TextChannel tc, MessageBuilder message) {
+		public void sendMessage(TextChannel tc, MessageBuilder message) {
 			message.setTTS(ttsMode);
 			tc.sendMessage(message.build()).complete();
 		}
+		
+		//Version 1.1.0
+		
+		public static void main(String[] args) {
+			args = new String[] {"true"};
+			String LIVE_TOKEN = "MzY3NDk2MzI5OTQ3MTE5NjE4.DL8SHQ.nW_rtXFgD7ytS3j7_lzZqxb4D5c", TEST_TOKEN = "MzY4MjE2MjU0Njg0NTk0MTc2.DMGvnw.gm57DR4Ado7zYE9M75zBI9x-38c";
+			String correctToken;
+			if(args.length == 0) {
+				correctToken = TEST_TOKEN;
+			} else {
+				try {
+					boolean val = Boolean.parseBoolean(args[0]);
+					if(val) {
+						correctToken = LIVE_TOKEN;
+					} else {
+						correctToken = TEST_TOKEN;
+					}
+				} catch (Exception e) {
+					correctToken = TEST_TOKEN;
+				}
+			}
+			
+			MDCBot newBot = new MDCBot(correctToken);
+			
+			try {
+				newBot.login();
+			} catch (LoginException | IllegalArgumentException | InterruptedException | RateLimitedException e) {
+				e.printStackTrace();
+				System.out.println("Unable to log in! Yikes!");
+			}
+			
+		}
+		
 	
 }
