6e70b81cda813780696f677e577053b3adc81505
Zach | xDest <destroyer7712@gmail.com>
2017-11-05 21:19:47 -0500

    Version command
    Needs to be manually updated every version

---
+++ MDCBot/src/main/java/com/mdc/bot/command/VersionCommand.java
@@ -0,0 +1,25 @@
+package com.mdc.bot.command;
+
+import com.mdc.bot.MDCBot;
+
+public class VersionCommand implements Command {
+
+	@Override
+	public boolean called(CommandSet s, MDCBot b) {
+		return true;
+	}
+
+	@Override
+	public void action(CommandSet s, MDCBot b) {
+		//Try to load from build.gradle?
+		//Doesn't work, just include internal version and REMEMBER to change it
+		String version = "Version: "+b.getVersion();
+		b.sendMessage(s.getTextChannel(), version);
+	}
+
+	@Override
+	public String getHelpMessage() {
+		return "Usage: `--version`";
+	}
+
+}
